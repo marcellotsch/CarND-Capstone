66b71a1a550ce5968989754223b6a61873203045
marcellotsch <marcellotsch@gmx.de>
2017-09-19 20:56:51 +0200

    Modified Waypoint Updater (Partial)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32 # used for traffic_waypoint subscribtion
 
 import math
 
@@ -28,25 +29,69 @@ class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb, queue_size=1)
+        
+        # Subriber for /traffic_waypoint
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb, queue_size=1)
+        # TODO: Subscriber for /obstacle_waypoint
+        #rospy.Subscriber('/obstacle_waypoint', ???, self.obstacle_cb, queue_size=1)
 
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
 
+        # Array to store waypoints data; is later added to lane object
+        self.waypoints_data = []
+
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+
+    		# Lane object (node output)
+        myLane = Lane()
+
+        # Current vehicle position
+        current_x = msg.pose.position.x
+        current_y = msg.pose.position.y
+
+        # Search nearest waypoint
+        min_dist = 9999999
+        nearest_index = -1
+        for i in range(len(self.waypoints_data)):
+        	pos_x = self.waypoints_data[i].pose.pose.position.x
+        	pos_y = self.waypoints_data[i].pose.pose.position.y
+        	if (self.dst(pos_x, pos_y, current_x, current_y) < min_dist):
+        		min_dist = self.dst(pos_x, pos_y, current_x, current_y)
+        		nearest_index = i
+        		#print("min_dist ", min_dist) # just for debugging
+
+        # Nearest waypoint could also lie behind the car... if so: consider the next waypoint in line
+        increase_index = False
+        nearest_x = self.waypoints_data[nearest_index].pose.pose.position.x
+        nearest_y = self.waypoints_data[nearest_index].pose.pose.position.y
+        nearest_next_x = self.waypoints_data[nearest_index+1].pose.pose.position.x
+        nearest_next_y = self.waypoints_data[nearest_index+1].pose.pose.position.y
+        if (((nearest_next_x-nearest_x>0.0) and (current_x>nearest_x)) or ((nearest_next_y-nearest_y>0.0) and (current_y>nearest_y)) or
+        	((nearest_next_x-nearest_x<0.0) and (current_x<nearest_x)) or ((nearest_next_y-nearest_y<0.0) and (current_y<nearest_y))):
+        	increase_index = True
+        if (increase_index):
+        	nearest_index+=1
+
+        # Provide info to lane object; ensure not to exceed max index of waypoints_data
+        myLane.waypoints = self.waypoints_data[nearest_index:min(nearest_index+LOOKAHEAD_WPS,len(self.waypoints_data)-nearest_index-1)]
+        self.final_waypoints_pub.publish(myLane)
+
+        # DEBUGGING
+        #print("current_x | current_y =", current_x, " | ", current_y)
+        #print("min_dist = ", min_dist)
+        #print("waypoints_data[nearest_index].x ", self.waypoints_data[nearest_index].pose.pose.position.x)
+        #print("myLane.waypoints = ", myLane.waypoints)
+
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.waypoints_data = waypoints.waypoints;
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -68,6 +113,11 @@ class WaypointUpdater(object):
         for i in range(wp1, wp2+1):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
+        return dist
+
+    def dst(self, x1, y1, x2, y2):
+    		# Calculate distance between two points
+        dist = math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2))
         return dist
 
 
